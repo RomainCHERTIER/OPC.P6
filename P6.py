d78a774ed037240cb0cc6374cf3fc692d3996218
Romain CHERTIER <romain.chertier@gmail.com>
2021-02-05 12:26:51 +0100

    Update P6.py

--- P6.py
+++ P6.py
@@ -3,9 +3,12 @@ import getpass
 import csv
 import random
 import datetime
+import ldap
 
 
-
+LDAP_HOST = "ldap://localhost"
+LDAP_BASE_DN = "ou=Users,dc=P6,dc=local"
+LDAP_ADMIN_DN = "cn=Administrateur,dc=P6,dc=local"
 logDir=""
 logFile="logP6_"
 logFileExt=".log"
@@ -55,6 +58,24 @@ def input_ldap_pass():
         logExit("ERROR", "erreur de saisie de mot de passe")
         return ''
 #
+#
+# try_ldap_bind
+#
+# Connexion au serveur Ldap
+# @param string admin_pass
+#
+#
+def try_ldap_bind(admin_pass):
+    try:
+        ldap_conn = ldap.initialize(LDAP_HOST)
+        ldap_conn.simple_bind_s(LDAP_ADMIN_DN, admin_pass)
+    except ldap.SERVER_DOWN:
+        logExit("ERROR", "Can't contact LDAP server")
+    except ldap.INVALID_CREDENTIALS:
+        logExit("ERROR", "This password is incorrect!")
+    log("INFO", "Authentication successful")
+
+#
 # read_csv
 #
 # lecture du fichier csv
@@ -92,6 +113,19 @@ def createPwd(pwdLevel):
     pwd = ""
     for i in range(nbCars): pwd = pwd + element[random.randint(0, len(element) - 1)]
 
+    return pwd
+#
+# addUser
+#
+# Ajout d'un utilisteur dans un LDAP
+# @param string surname,name,login,pwdLevel valeur du tableau
+# @return arrey Mot de passe
+#
+#
+def addUser(surname,name,login,pwdLevel):
+    pwd = createPwd(pwdLevel)
+    print (name+" "+surname+" "+pwd)
+    raise Exception ("erreur Ldap")
     return pwd
 
 #

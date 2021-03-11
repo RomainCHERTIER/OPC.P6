28e9aaf5d1595faa8ac064cb5488e39fb8ab0d80
Romain CHERTIER <romain.chertier@gmail.com>
2021-03-11 23:50:45 +0100

    Livrable

--- P6.py
+++ P6.py
@@ -1,8 +1,27 @@
+###############################################################################
+#MIT License
+
+#Copyright (c) 2021 Romain CHERTIER
+#opensource.org/licenses/mit-license.php
+
+###############################################################################
+#
+#                    Script for creating user accounts
+#
+# description scrip
+#
+#
+# @author Romain CHERTIER<romain.chertier@sfr.fr>
+# @version 1.1
+# @date 2021-03-01
+#
+
 import getpass
 import csv
 import random
 import datetime
 import ldap
+from ldap3 import *
 
 
 ################################   Constants   ################################
@@ -12,6 +31,9 @@ dataCsvFile = "csvp6.csv"
 LDAP_HOST = "ldap://localhost"
 LDAP_BASE_DN = "cn=Users,dc=P6,dc=local"
 LDAP_ADMIN_DN = "cn=Administrateur,cn=Users,dc=P6,dc=local"
+
+domain = 'P6.local'
+logAdmin = 'Administrateur'
 
 logDir = ""
 logFile = "logP6_"
@@ -73,7 +95,6 @@ def input_ldap_pass():
         return getpass.getpass("Enter LDAP manager password:")
     except getpass.GetPassWarning:
         logExit("ERROR", "erreur de saisie de mot de passe")
-        return ""
 
 
 #
@@ -147,33 +168,38 @@ def createPwd(pwdLevel):
 # @return arrey pwd
 #
 #
-def addUser(surname, name, login, pwdLevel):
+def addUser(surname, name, login, pwdLevel, domain):
     pwd = createPwd(pwdLevel)
     print(name + " " + surname + " " + pwd)
     fullname = surname + " " + name
     dn = "cn=" + fullname + "," + LDAP_BASE_DN
-    entry = [
-        ("objectClass", ["person".encode("utf-8"),
-                         "organizationalPerson".encode("utf-8"),
-                         "user".encode("utf-8"),
-                         "posixAccount".encode("utf-8"),
-                         "top".encode("utf-8"),
-                         ]),
-        ("uid", login.encode("utf-8")),
-        ("cn", fullname.encode("utf-8")),
-        ("givenname", surname.encode("utf-8")),
-        ("sn", name.encode("utf-8")),
-        ("userPassword", pwd.encode("utf-8"))
-    ]
+    upn = login + "@" + domain
     try:
-        ldap_conn = ldap.initialize(LDAP_HOST)
-        ldap_conn.simple_bind_s(LDAP_ADMIN_DN, ldapPwd)
-        ldap_conn.add_s(dn, entry)
+        # connect - specifying port 636 is only for reference as it's inferred
+        s = Server('ldaps://localhost:636', connect_timeout=5)
+        c = Connection(s, user='{}\{}'.format(domain, logAdmin), password=ldapPwd, authentication="NTLM")
+
+        if not c.bind():
+            exit(c.result)
+        else :
+            # create user
+            c.add(dn, attributes={
+                'objectClass': ['organizationalPerson', 'person', 'top', 'user'],
+                'sAMAccountName': login,
+                'userPrincipalName': "{}@{}".format(login, domain),
+                'displayName': login
+            })
+            # set password - must be done before enabling user
+            # you must connect with SSL to set the password
+            c.extend.microsoft.modify_password(dn, pwd)
+
+            # enable user (after password set)
+            c.modify(dn, {'userAccountControl': [('MODIFY_REPLACE', 512)]})
+            return pwd
+
     except Exception as err:
-        log("ERROR", "adUser:" + login + " erreur : " + format(err) + "\n")
-    finally:
-        ldap_conn.unbind_s()
-    return pwd
+        log("ERROR", "adUser:" + login + " erreur : " + format(err))
+        return None
 
 
 #
@@ -189,13 +215,15 @@ def doInscription(listCSV):
     for i in range(nbEnrgs):
         enrg = listCSV[i]
         try:
-            pwd = addUser(enrg["Nom"], enrg["Prenom"], enrg["NomUtilisateur"], enrg["NiveauMDP"])
+            pwd = addUser(enrg["Nom"], enrg["Prenom"], enrg["NomUtilisateur"], enrg["NiveauMDP"], enrg["Domaine"])
+            if pwd is None:
+               raise Exception("Erreur lors de l'inscription")
             storePwd(enrg["NomUtilisateur"], pwd)
         except Exception as err:
-            log("ERROR", "erreur durant l'inscription de " + enrg["NomUtilisateur"] + " erreur : " + format(err) + "\n")
+            log("ERROR", "erreur durant l'inscription de " + enrg["NomUtilisateur"] + " erreur : " + format(err))
             nbError = nbError + 1
         else:
-            log("INFO", "inscription de " + enrg["NomUtilisateur"] + " : OK\n")
+            log("INFO", "inscription de " + enrg["NomUtilisateur"] + " : OK")
     return nbError
 
 
@@ -208,7 +236,7 @@ def doInscription(listCSV):
 #
 def storePwd(nomUtilisateur, pwd):
     now = datetime.datetime.now()
-    line = now.strftime("%Y%m%d %H%M%S") + "inscription de " + nomUtilisateur + " " + pwd + "\n"
+    line = now.strftime("%Y%m%d %H%M%S") + " inscription de " + nomUtilisateur + " " + pwd + "\n"
     dateYYYYMMDD = now.strftime("%Y%m%d")
     userAbsPath = userDir + userFile + dateYYYYMMDD + userFileExt
     try:
@@ -216,7 +244,7 @@ def storePwd(nomUtilisateur, pwd):
         fd.write(line + "\n")
         fd.close()
     except Exception as err:
-        print(" ERROR : l'utilisateur " + nomUtilisateur + " n'a pas été créé " + "\n")
+        log("ERROR", "l'utilisateur" + nomUtilisateur + "n'a pas été créé")
 
 
 ##################################   Main   ###################################
